4eb2966aa697cd595c34be08629058ec0e5c9f6f
Platinio <platinio94@gmail.com>
2018-10-14 19:47:13 -0300

    add rotation tween

---
+++ Assets/Platinio/Demo/Scripts/RotationExample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Platinio.TweenEngine;
+
+namespace Demo
+{
+    public class RotationExample : MonoBehaviour
+    {
+        private void Awake()
+        {
+            PlatinioTween.instance.RotateTween(transform , Vector3.forward , 90.0f , 3.0f).SetEase(Ease.EaseOutElastic).SetDelay(2.0f);
+        }
+    }
+
+}
+
