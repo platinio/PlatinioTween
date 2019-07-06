a8560aa3b0288df0fb3a6bb300e1fed15196e719
Platinio <platinio94@gmail.com>
2019-07-06 14:54:07 -0400

    adding resources for md file

---
+++ Assets/Platinio/Examples/Examples/Scripts/EaseShowcase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platinio.TweenEngine
+{
+    public class EaseShowcase : MonoBehaviour
+    {
+        [SerializeField] private Ease ease;
+        [SerializeField] private float t;
+        [SerializeField] private Vector3 pos;
+
+        private void Awake()
+        {
+            PlatinioTween.instance.Move(transform , pos , t).SetEase(ease);
+        }
+       
+    }
+
+}
+
