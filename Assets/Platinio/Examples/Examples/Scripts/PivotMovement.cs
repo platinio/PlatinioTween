7c518d8b0280b061c91091eb9d323419a2d24fc2
Platinio <platinio94@gmail.com>
2019-07-06 19:08:31 -0400

    adding examples in anchors

---
+++ Assets/Platinio/Examples/Examples/Scripts/PivotMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platinio.TweenEngine;
+
+namespace Platinio
+{
+    public class PivotMovement : MonoBehaviour
+    {
+        [SerializeField] private Vector2 finalPos = Vector2.zero;
+        [SerializeField] private PivotPreset anchor = PivotPreset.MiddleCenter;
+        [SerializeField] private RectTransform canvas = null;
+        
+
+        private void Start()
+        {
+            PlatinioTween.instance.MoveUI(GetComponent<RectTransform>() , finalPos, canvas , 0.5f , anchor);
+        }
+      
+    }
+
+}
