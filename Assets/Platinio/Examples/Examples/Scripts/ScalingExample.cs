4128a5de30297cd6d76b858c92d96fcd79e7284e
Platinio <platinio94@gmail.com>
2019-08-04 11:09:36 -0400

    update scaling example

--- Assets/Platinio/Examples/Examples/Scripts/ScalingExample.cs
+++ Assets/Platinio/Examples/Examples/Scripts/ScalingExample.cs
@@ -7,9 +7,28 @@ namespace Demo
     {
         [SerializeField] private GameObject m_go = null;
 
+        private float targetScale = 0.0f;
+
+
         private void Awake()
         {
-            PlatinioTween.instance.ScaleTween(m_go.transform , Vector3.one * 8 , 1.5f).SetEase(Ease.EaseOutElastic).SetDelay(2.0f);
+            Scale();
+        }
+
+        private void Scale()
+        {
+            float t = 1.5f;
+
+            targetScale += 5.0f;
+
+            if (targetScale > 40.0f)
+            {
+                t = 5.0f;
+                targetScale = 5.0f;
+            }
+                
+
+            PlatinioTween.instance.ScaleTween( m_go.transform, Vector3.one * targetScale, t ).SetEase( Ease.EaseOutElastic ).SetDelay( 0.2f ).SetOnComplete(Scale);
         }
     }
 
