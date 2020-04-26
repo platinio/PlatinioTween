38bd56bd273351021630a3e3a409ff6657fc0657
James Roman <platinio94@gmail.com>
2020-04-26 15:31:41 -0400

    remove unused piece of code from ColorTween

--- Assets/PlatinioTween/Scripts/Core/Tweens/ColorTween.cs
+++ Assets/PlatinioTween/Scripts/Core/Tweens/ColorTween.cs
@@ -40,18 +40,7 @@ namespace Platinio.TweenEngine
                 return;
             }
 
-            /*
-            Vector3 to = new Vector3( this.to.r, this.to.g, this.to.b );
-            Vector3 from = new Vector3( this.from.r, this.from.g, this.from.b );
-
-            //get new value
-            Vector3 change = to - from;
-            Vector3 value = Equations.ChangeVector( currentTime, from, change, duration, ease );
-
-            //get new value
-            float alphaChange = this.to.a - this.from.a;
-            float alphaValue = Equations.ChangeFloat( currentTime, this.from.a, alphaChange, duration, ease );
-            */
+            
             Color color = Color.LerpUnclamped(from, to, EasingFunctions.ChangeFloat(0.0f, 1.0f, currentTime / duration, ease));
 
             //call update if we have it
